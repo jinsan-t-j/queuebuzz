6767a3152b3efd6d15fc9d2bde6b0f68a626c16b
Jinsan TJ <jinsantj.devs@gmail.com>
2026-05-02 03:47:19 +0530

    feat: Removed recovery email for anonymous hosts

--- internal/modules/queue/dto/request.go
+++ internal/modules/queue/dto/request.go
@@ -8,14 +8,12 @@ type CreateQueueRequest struct {
 	AvgServiceMins    *int    `json:"avg_service_mins" validate:"omitempty,min=1,max=60"`
 	AllowPartyJoining *bool   `json:"allow_party_joining" validate:"omitempty"`
 	MaxPartySize      *int    `json:"max_party_size" validate:"omitempty,min=1,max=100"`
-	RecoveryEmail     *string `json:"recovery_email" validate:"omitempty,email"`
 	CollectEmails     *bool   `json:"collect_emails" validate:"omitempty"`
 }
 
 type UpdateQueueRequest struct {
 	Name              *string `json:"name" validate:"omitempty,min=3,max=50"`
 	AvgServiceMins    *int    `json:"avg_service_mins" validate:"omitempty,min=1,max=60"`
-	RecoveryEmail     *string `json:"recovery_email" validate:"omitempty,email"`
 	Slug              *string `json:"slug" validate:"omitempty,min=3,max=20,slug"`
 	AllowPartyJoining *bool   `json:"allow_party_joining" validate:"omitempty"`
 	MaxPartySize      *int    `json:"max_party_size" validate:"omitempty,min=1,max=100"`
